bb439527311881cb1c9c8baa73542d212b98d49a
asupan <a.tkn25vv@gmail.com>
2026-06-02 22:27:25 +0900

    index.blade.php 修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -16,7 +16,7 @@
         <div class="shop__text">
             <h2 class="shop__title">{{ $shop['title'] }}</h2>
             <span class="shop__area">#{{ $shop['area']['name'] }}</span>
-            <s class="shop__genre">#{{ $shop['genre']['name'] }}</s
+            <span class="shop__genre">#{{ $shop['genre']['name'] }}</span>
             <div class="buttons">
                 <form class="detail-form" action="/detail/{shop_id}" method="get">
                     @csrf
