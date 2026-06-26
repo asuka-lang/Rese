f0f11b2677c7ebe751e45a020cd682e00c24070d
asupan <a.tkn25vv@gmail.com>
2026-06-26 11:58:59 +0900

    Stripe関連ファイル修正

--- src/app/Http/Controllers/PaymentController.php
+++ src/app/Http/Controllers/PaymentController.php
@@ -9,10 +9,10 @@ use App\Models\Reserve;
 
 class PaymentController extends Controller
 {
-    public function checkout($reservation_id)
+    public function checkout(Reserve $reserve)
     {
         // 予約情報を取得
-        $reservation = Reserve::with('shop')->findOrFail($reservation_id);
+        $reservation = $reserve->load('shop');
         // Stripe 鍵設定
         Stripe::setApiKey(config('services.stripe.secret'));
         // Checkout セッション作成
@@ -22,9 +22,9 @@ class PaymentController extends Controller
                 'price_data' => [
                     'currency' => 'jpy',
                     'product_data' => [
-                        'name' => $reservation['shop']['title'].'利用料金',
+                        'name' => $reservation->shop->title.'利用料金',
                     ],
-                    'unit_amount' => $reservation['price'], //金額 予約ごとに変動
+                    'unit_amount' => $reservation->price, //金額 予約ごとに変動
                 ],
                 'quantity' => 1,
             ]],
